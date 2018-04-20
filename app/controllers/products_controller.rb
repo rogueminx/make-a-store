f9e9626a3dba9e7d3c5eb6b897e3e1cf37f9b494
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-04-20 12:03:41 -0700

    add product display page with all product info

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,9 @@ class ProductsController < ApplicationController
     @recent_products = Product.recently_added
     @reviewed_products = Product.most_reviews
   end
+
+  def show
+    @product = Product.find(params[:id])
+    render :display
+  end
 end
