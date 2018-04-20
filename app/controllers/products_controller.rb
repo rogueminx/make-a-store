bf6bfc5c2423aa4158093693f4cb846e64745adb
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-04-20 14:00:47 -0700

    successfully create new product listing add error panel to layouts.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,20 @@ class ProductsController < ApplicationController
     render :display
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "Congratulations! Your #{@product.name} was added to the product list"
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
   def edit
     @product = Product.find(params[:id])
   end
