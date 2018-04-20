944beed89f3d7630f69001c9446bdf6bd178a9a5
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-04-20 13:02:38 -0700

    add edit update and destroy to product controller. :rose:

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,24 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     render :display
   end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update!(product_params)
+      flash[:notice] = "Good job! Your product was updated!"
+      redirect_to product_path(@product)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
 end
