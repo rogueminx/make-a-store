bc3ca4ef3f6915fba568da974c6580e903cac061
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-04-20 08:55:47 -0700

    meaningful fail test for review author and description.

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 describe Review do
   it { should validate_presence_of :rating }
+  it { should validate_presence_of :author }
+  it { should validate_presence_of :description }
 end
