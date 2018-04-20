b020154628a42dd0e343df14400c542d9834386b
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-04-20 09:13:10 -0700

    validates the length of the review description.

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ActiveRecord::Base
   validates :rating, :presence => true
   validates :author, :presence => true
   validates :description, :presence => true
+  validates_length_of :description, :minimum => 50, :maximum => 250
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,4 +4,14 @@ describe Review do
   it { should validate_presence_of :rating }
   it { should validate_presence_of :author }
   it { should validate_presence_of :description }
+
+  it("validates length of description is between 50 and 250 charachters") do
+    review = Review.new({:rating => 3, :author => "Amy", :description => "a".*(25)})
+    expect(review.save()).to(eq(false))
+  end
+
+  it("validates length of description is between 50 and 250 charachters") do
+    review = Review.new({:rating => 3, :author => "Amy", :description => "a".*(150)})
+    expect(review.save()).to(eq(true))
+  end
 end
