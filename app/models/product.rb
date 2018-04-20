c894d010c3413e7d13342fe611680b71b9b24153
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-04-20 09:32:16 -0700

    pass test to check for presence of product info.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ActiveRecord::Base
   validates :name, :presence => true
+  validates :origin, :presence => true
+  validates :variety, :presence => true
+  validates :notes, :presence => true
+  validates :price, :presence => true
 end
