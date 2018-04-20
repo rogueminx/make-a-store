edbaaac921a1db3b14e091e8ce00a7276a9d9a39
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-04-20 09:31:01 -0700

    fail validations for presence of Product info.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 describe Product do
   it { should validate_presence_of :name }
+  it { should validate_presence_of :origin }
+  it { should validate_presence_of :variety }
+  it { should validate_presence_of :notes }
+  it { should validate_presence_of :price }
 end
