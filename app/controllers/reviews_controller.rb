d3af09100fe46bdca98f79cc620ba6e429f18084
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-04-20 16:20:54 -0700

    create and display new reviews. :thumbsup:

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,4 +5,20 @@ class ReviewsController < ApplicationController
     @review = Review.new
   end
 
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      flash[:notice] = "Thanks, your review is has been added to the list!"
+      redirect_to product_path(@product.id)
+    else
+      render :new
+    end
+  end
+
+private
+  def review_params
+    params.require(:review).permit(:author, :rating, :description)
+  end
+
 end
