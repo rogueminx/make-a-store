b5cad944551f00d0f5e02de15846d2078ac55970
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-04-20 09:54:29 -0700

    titleize product name, origin and variety before saving.

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,12 @@ class Product < ActiveRecord::Base
   validates :variety, :presence => true
   validates :notes, :presence => true
   validates :price, :presence => true, format: { with: /\A\d+(?:\.\d{2})?\z/ }, numericality: { greater_than: 0, less_than: 500 }
+  before_save :titleize_entries
+
+private
+  def titleize_entries
+    self.name = self.name.titleize
+    self.origin = self.origin.titleize
+    self.variety = self.variety.titleize
+  end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -8,12 +8,19 @@ describe Product do
   it { should validate_presence_of :price }
 
   it("validates format of price is less than $500 and has two decimal places") do
-    product = Product.new({:name => "Coffee", :origin => "Guatemala", :variety => "Single Origin", :notes => "Very yummy coffee notes.", :price => 22.232})
+    product = Product.new({:name => "Coffee", :origin => "Guatemala", :variety => "Single Origin", :notes => "Very yummy coffee notes.", :price => "22.232"})
     expect(product.save()).to(eq(false))
   end
 
   it("validates format of price is less than $500 and has two decimal places") do
     product = Product.new({:name => "Coffee", :origin => "Guatemala", :variety => "Single Origin", :notes => "Very yummy coffee notes.", :price => "54.10"})
     expect(product.save()).to(eq(true))
+  end
+
+  it("will titleize name, origin and variety before saving") do
+    product = Product.create({:name => "coffee", :origin => "guatemala", :variety => "single Origin", :notes => "Very yummy coffee notes.", :price => "54.10"})
+    expect(product.name()).to(eq("Coffee"))
+    expect(product.origin()).to(eq("Guatemala"))
+    expect(product.variety()).to(eq("Single Origin"))
   end
 end
