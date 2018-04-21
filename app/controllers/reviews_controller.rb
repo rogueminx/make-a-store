9b395d0ca1927a00219f9563d23a325a98a77b6d
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-04-20 17:04:30 -0700

    update and delete revoews correctly.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -16,6 +16,30 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    if @review.update!(review_params)
+      flash[:notice] = "Your product was updated!"
+      redirect_to product_path(@product.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @product_id = @review.product_id
+    @review.destroy
+    redirect_to product_path(@product_id)
+  end
+
+
 private
   def review_params
     params.require(:review).permit(:author, :rating, :description)
